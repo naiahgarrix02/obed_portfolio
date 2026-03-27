6df52eb75d081f65e75c29a5debf85d1a1d89136
Benaiah Odametey <101115740+naiahgarrix02@users.noreply.github.com>
2026-03-27 00:34:07 +0000

    commit of services section

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,9 +1,11 @@
+import { services } from "../constants";
+
 const Services = () => {
   return (
-    <div className="bg-[url('/bg_img.jpg')] bg-cover bg-center min-h-screen flex flex-col justify-between px-15 pt-30">
+    <div className="bg-[url('/bg_img.jpg')] bg-cover bg-center min-h-screen flex flex-col justify-between px-15 pt-30 pb-50">
       <div className="flex flex-row justify-between items-start align-center">
         <h2 className="font-outfit-bold text-[64px] text-[#FDB12F] m-0 ">
-          WHAT DO I OFFER
+          WHAT DO I OFFER?
         </h2>
         <div className="flex flex-row justify-end gap-16 items-center align-center">
           <p className="text-[24px] font-outfit-regular text-white w-7/10">
@@ -19,6 +21,41 @@ const Services = () => {
               <img src="/arrow.svg" alt="arrow" className="w-6 h-6" />
             </div>
           </a>
+        </div>
+      </div>
+
+      <div className="w-full mt-20 justify-end">
+        <div className="flex flex-row justify-end gap-20 items-end align-end p-10">
+          {services.map((service) => (
+            <div
+              key={service.title}
+              className="group flex flex-col items-start gap-14 w-100 h-118 
+               transition-all duration-300 
+               hover:bg-[#FDB12F] pl-11 pt-16 pb-16 hover:translate-y-2"
+            >
+              <div className="flex flex-col gap-12">
+                <img
+                  src={service.icon}
+                  alt={service.title}
+                  className="w-16 h-16  group-hover:invert"
+                />
+
+                <h3
+                  className="font-outfit-regular text-[36px] text-white 
+                     group-hover:text-black w-1/3"
+                >
+                  {service.title}
+                </h3>
+              </div>
+
+              <p
+                className="text-[20px] font-outfit-light text-white 
+                  group-hover:text-black w-4/5"
+              >
+                {service.description}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/constants/index.tsx
@@ -0,0 +1,26 @@
+export type Services = {
+  title: string;
+  description: string;
+  icon: string;
+};
+
+export const services: Services[] = [
+  {
+    title: "APPLICATION DEVELOPMENT",
+    description:
+      "I transform your ideas into engaging mobile apps with a keen focus on user experience",
+    icon: "/app_dev.svg",
+  },
+  {
+    title: "UI/UX DESIGN",
+    description:
+      "I bring your creative visions to life with beautiful designs that leave a lasting impression",
+    icon: "/ui_ux.svg",
+  },
+  {
+    title: "WEB DEVELOPMENT",
+    description:
+      "I make websites that bring your unique ideas to life with beautiful interfaces",
+    icon: "/web_dev.svg",
+  },
+];
